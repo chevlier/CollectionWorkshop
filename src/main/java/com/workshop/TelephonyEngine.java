909281f7bb04b058c7c9d7e980094a650d7cae5e
chevlier <milihenr@gmail.com>
2019-09-02 15:34:36 +0200

    Brave structure to test.

---
+++ src/main/java/com/workshop/TelephonyEngine.java
@@ -0,0 +1,21 @@
+package com.workshop;
+
+
+
+public class TelephonyEngine {
+    private int telephonyChannel = 560;
+
+    TelephonyCall myCall = new TelephonyCall();
+
+    public void addTelephonyChannel() {
+        myCall.setChannelId(telephonyChannel);
+    }
+
+    public boolean checkChannelAvailability(){
+        addTelephonyChannel();
+        if (myCall.getChannelId()%2 ==0){
+            return true;}
+        else {
+            return false;}
+    }
+}
